7a5e13b5d20d86f37ad608214dd41f6bf1f317a3
ixnp <enprocopios@gmail.com>
2019-07-25 11:55:08 -0700

    get part of solution done

---
+++ src/index.js
@@ -0,0 +1,63 @@
+API_URL = 'http://localhost:3000/profile'
+//parent function that loads everything
+function displayPage(){
+    loadprofile()
+    .then(data => createPageEl(data))
+}
+function loadprofile(){
+    return fetch(API_URL)
+    .then(res => res.json())
+    .then(data => data)
+}
+
+function createPageEl(data){
+    console.log(data)
+    buildPortfolioHeader(data)
+    data.post.forEach(post => buildPosts(post))
+}
+
+function buildPortfolioHeader(data){
+    console.log(data.name)
+    let headerH1 = document.getElementById('name')
+    let headerImg = document.getElementById('avatar')
+    headerH1.innerText = data.name
+    headerImg.innerText = data.avatar
+}
+function buildPosts(post){
+    console.log(post.content)
+    let postDiv = document.createElement('div')
+    postDiv.id = 'single-post-container'
+    let postContent = document.createElement('h3')
+    postContent.innerText = post.content
+    //add event listener to make update
+    let postlikeButton = document.createElement('button')
+    postlikeButton.innerText = post.likes
+    let portfolio = document.querySelector('section')
+    portfolio.appendChild(postDiv)
+    let postContainer = document.getElementById('single-post-container')
+    postContainer.appendChild(postContent)
+    postContainer.appendChild(postlikeButton)
+    post.comments.forEach(comment => buildComments(comment))
+}
+
+function buildComments(comment){
+    let commentDiv = document.createElement('div')
+    commentDiv.id = 'comment-container'
+    let commentContent = document.createElement('p')
+    commentContent.innerText = comment.content
+    let commentLikes = document.createElement('button')
+    commentLikes.innerHTML = comment.likes
+    let postContainer = document.getElementById('single-post-container')
+    postContainer.appendChild(commentDiv)
+    let commentContainer = document.getElementById('comment-container')
+    commentContainer.appendChild(commentContent)
+    commentContainer.appendChild(commentLikes)
+}
+function main(){
+    displayPage()
+}
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+
+    main();
+})
